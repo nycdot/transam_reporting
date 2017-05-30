1a16ab14d94912797afb5feff6570652c2929384
W. Scott Meeks <smeeks@camsys.com>
2017-05-30 16:36:56 -0400

    Remove default as it was causing problems with the schema dumper and is not actually needed.

--- db/migrate/20170523185106_add_columns_to_reports.rb
+++ db/migrate/20170523185106_add_columns_to_reports.rb
@@ -1,6 +1,6 @@
 class AddColumnsToReports < ActiveRecord::Migration
   def change
-    add_column :reports, :printable, :boolean, null: false, default: false, after: :active
-    add_column :reports, :exportable, :boolean, null: false, default: false, after: :printable
+    add_column :reports, :printable, :boolean, null: false, after: :active
+    add_column :reports, :exportable, :boolean, null: false, after: :printable
   end
 end
