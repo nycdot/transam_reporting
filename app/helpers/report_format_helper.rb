4ba867a6cf7859fddd3ee21efc6d59233cbb714e
W. Scott Meeks <smeeks@camsys.com>
2017-06-08 17:48:21 -0400

    Add support for ALI Funding report. [#137134227]

--- app/helpers/report_format_helper.rb
+++ app/helpers/report_format_helper.rb
@@ -1,8 +1,18 @@
 module ReportFormatHelper
   def format_using_format(val, format)
     case format
-    when :currency
+    when :currencyM
       number_to_currency(val, format: '%u%nM', negative_format: '(%u%nM)')
+    when :currency
+      format_as_currency(val)
+    when :fiscal_year
+      format_as_fiscal_year(val)
+    when :integer
+      format_as_integer(val)
+    when :string
+      val
+    when :checkbox
+      format_as_checkbox(val)
     else
       # Note, current implementation uses rescue and is thus potentially inefficient.
       # Consider alterantives.
