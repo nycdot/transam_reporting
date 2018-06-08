2407419c015b46243ada2063d8293b7ea1c9faae
mathmerized <mathmerized@gmail.com>
2018-06-08 12:13:31 -0400

    write seed to match data migrations / other scripts

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+#encoding: utf-8
+
+# determine if we are using postgres or mysql
+is_mysql = (ActiveRecord::Base.configurations[Rails.env]['adapter'].include?'mysql2')
+is_sqlite =  (ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'sqlite3')
+
+puts "======= Updating TransAM CORE Reports  ======="
+
+reports = [
+    {
+        :name => 'User Login Report'
+    },
+    {
+        :name => 'Issues Report'
+    }
+]
+
+reports.each do |report|
+  Report.find_by(report).update!(printable: true, exportable: true)
+end
